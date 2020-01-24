437390f75e03521f7c6a544ad3982e176576b276
Junaid Baig <junaidb790@gmail.com>
2020-01-24 12:14:23 -0500

    calculated thetan and phin

--- tnpn.py
+++ tnpn.py
@@ -15,12 +15,17 @@ def phiN(g11,h11):
 def main():
     file = "CarringtonNumbersAndCoeff.csv"
     df = pd.read_csv(file, sep=",")
-
     #Calculates Bo, the square root of the sum of the squares of the WSO coefficents
     df['Bo'] = ((df['g10'] ** 2) + (df['g11'] ** 2) + (df['h11'] ** 2)) ** (1/2)
 
-    
+
+    df['thetaN'] = np.degrees(np.arccos(df['g10'].astype(float)/df['Bo'].astype(float)))
+
+    df['phiN'] = np.degrees(np.arctan2(df['h11'].astype(float), df['g11'].astype(float)))
+
     df.to_csv(file,index=False)
+
+    print(np.degrees(np.arccos(-29.95/86.56)))
 
 if __name__ == "__main__":
     main()
