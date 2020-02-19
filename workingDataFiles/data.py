fixed radians to degrees

workingDataFiles/data.py
@@ -1,24 +1,31 @@
 import pandas as pd
 import math
+import numpy as np
 
 def ThetaD(theta,phi,thetaN,phiN):
-    thetad = math.acos(((math.cos(thetaN)*math.cos(theta)) + (math.sin(thetaN)*math.sin(theta)*math.cos(phi - phiN)))) 
-    return thetad
+    #thetad = math.acos(((math.cos(thetaN)*math.cos(theta)) + (math.sin(thetaN)*math.sin(theta)*math.cos(phi - phiN))))
+
+    thetad2 = np.degrees(np.arccos(((np.cos(thetaN)*np.cos(theta)) +
+        (np.sin(thetaN)*np.sin(theta)*np.cos(phi - phiN)))))
+
+    return thetad2
     
 
 def PhiD(theta, thetaN, thetaD, phi, phiN):
     #phid = math.acos(-((math.cos(theta)-(math.cos(thetaN)*math.cos(thetaD))) / (math.sin(thetaN)*math.sin(thetaD))))
-    phid = math.asin((math.sin(theta)*math.sin(phi-phiN)) / math.sin(thetaD))
-    return phid
+    #phid = math.asin((math.sin(theta)*math.sin(phi-phiN)) / math.sin(thetaD))
+
+    phid2 = np.degrees(np.arcsin((np.sin(theta)*np.sin(phi-phiN)) / np.sin(thetaD)))
+
+    return phid2
 
 
 def main():
-    dfC = pd.read_csv("CarringtonNumbersAndCoeff.csv", sep = ',')
+    #dfC = pd.read_csv("CarringtonNumbersAndCoeff.csv", sep = ',')
     dfD = pd.read_csv("data.csv", sep = ',')
 
     N = len(dfD)
-    
-    '''
+    """
     c = 0
     for j in range(0,N):
         if(dfD.Rotation[j] == dfC.Rotation[c]):
@@ -31,17 +38,17 @@ def main():
                 dfD.PhiN[j] = dfC.PhiN[c]
 
         print(str(j))
-    '''
-    '''
+    """
+
     for j in range(0,N):
-        dfD.ThetaD[j] = ThetaD(dfD.Theta[j], dfD.Phi[j], dfD.ThetaN[j], dfD.PhiN[j])
+        dfD.ThetaD2[j] = ThetaD(dfD.Theta[j], dfD.Phi[j], dfD.ThetaN[j], dfD.PhiN[j])
         print("j = " + str(j) + " ThetaD[j]: " + str(dfD.ThetaD[j]))
 
     dfD.to_csv("data.csv", index = False)
-    '''
+    
 
     for k in range(0,N):
-        dfD.PhiD[k] = PhiD(dfD.Theta[k], dfD.ThetaN[k], dfD.ThetaD[k], dfD.Phi[k], dfD.PhiN[k])
+        dfD.PhiD2[k] = PhiD(dfD.Theta[k], dfD.ThetaN[k], dfD.ThetaD[k], dfD.Phi[k], dfD.PhiN[k])
         print("k = ", str(k) + " PhiD[k]: " + str(dfD.PhiD[k]))
     
     dfD.to_csv("data.csv", index = False)